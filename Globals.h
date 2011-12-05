cacdbf47ca9bf657f0ddf894a8456eb36daf007d
david <david@imesmes.com>
2011-12-05 13:09:41 +0100

    WIP: Basic SceneManager

--- Globals.h
+++ Globals.h
@@ -3,7 +3,16 @@
 
 #include <GL/glut.h>
 #include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <map>
+#include <utility>
+
+using namespace std;
+
 #include "Game.h"
+#include "Scene.h"
+#include "SceneManager.h"
 
 #define GAME_WIDTH 640
 #define GAME_HEIGHT 480
